d90dd7a992d5def599580af2b3fa1bd0f0f6f7d1
“Sarah <sarahcatherine311@gmail.com>
2023-01-11 20:28:24 -0700

    Add final update

--- friends.js
+++ friends.js
@@ -1,7 +1,11 @@
-var besties = ["Megan"];
+var besties = "Tom";
+
+var goodFriend = "Bob"
 
 if (besties === "Megan") {
     console.log ("Hi best friend!")
+} else if (goodFriend === "Bob") {
+    console.log ("Hi good friend")
 } else {
     console.log ("Hi friend")
 }
